be49d18efd89dd3e50062ea917d1dfacec34d958
Kaan Kani İnce <kkince@newmind.ai>
2024-08-28 16:48:53 +0300

    message routes and controllers added

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
+import protectRoute from "../middleware/protectRoute.js";
+import {sendMessage, getMessages, getUsersForSidebar} from "../controllers/message.controller.js";
 const router = express.Router();
 
-router.get("/conversations", (req, res) => {
-    res.send("Conversation Route")
-});
+router.post("/send/:id", protectRoute,sendMessage)
+router.get("/conversations", protectRoute,getUsersForSidebar)
+router.get("/:id", protectRoute,getMessages)
 
 export default router;
